6e7b2c0fe577dc350e7da09d779b3fda539a271e
Evandro Myller <evandromyller@gmail.com>
2009-12-28 16:01:29 -0300

    widget polished; now selecting months/dates

--- module.js
+++ module.js
@@ -4,7 +4,9 @@ u.require('effects');
 
 var LC = {
 	MONTHS: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-	DAYS: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] }
+	DAYS: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+	formatDate: function (date) {
+		return date.getFullYear()+"-"+(date.getMonth() + 1)+"-"+date.getDate(); }}
 
 new u.Module("calendar", { version: .1, hasCSS: !0 },
 // core
@@ -30,18 +32,56 @@ new u.Module("calendar", { version: .1, hasCSS: !0 },
 			weeks.append("th", LC.DAYS[w].slice(0, 2));
 
 		var this_ = this;
-		u(field).on('focus', u.Calendar.open);
-		this.DOM.buttons.prev.on('click', function () { this_.prevMonth(); });
-		this.DOM.buttons.next.on('click', function () { this_.nextMonth(); });
+		u(field).on('focus', function () { this_.open(); });
+		this.DOM.buttons.prev.on('click', function () { this_.prevPage(); });
+		this.DOM.buttons.next.on('click', function () { this_.nextPage(); });
+		this.DOM.main.on('click', function (e) { this_.select(e); });
+		this.DOM.switcher.on('click', function () {
+			if (this_.current.month != undefined)
+				this_.DOM.month.suck({ duration: 300 }, function () {
+					this_.DOM.year.show();
+					this_.year(this_.current.year); });
+		});
 	},
 
-	$open: function () {
-		var c = this.calendar;
-		u("body").append(c.DOM.main).pos(['left', 'bottom+height'], this).show();
-		!c.DOM.switcher.text() && c.goTo(c.current.date); },
+	open: function () {
+		u("body").append(this.DOM.main).pos(['left', 'bottom+height'], this.element).show();
+		!this.DOM.switcher.text() && this.goTo(this.current.date); },
+
+	close: function () {
+		this.DOM.main.fadeOut({ duration: 200, destroy: !0 }); },
+
+	select: function (e) {
+		var this_ = this;
+		if (e.target.parentNode.nodeName == "TD")
+			if (this.current.month != undefined) {
+				this.current.day = +u(e.target).text();
+				this.element.value = LC.formatDate(new Date(this.current.year, this.current.month, this.current.day));
+				this.close(); }
+			else
+			if (this.current.year != undefined)
+				this.DOM.year.puff({ duration: 300 }, function () {
+					this_.DOM.month.show();
+					this_.month(this_.current.year, u("button", this_.DOM.year).indexOf(e.target))
+				})
+	},
 
 	goTo: function (date) {
 		this.month(date.getFullYear(), date.getMonth()); },
+
+	year: function (year) {
+		this.current.year = year;
+		this.current.month = undefined;
+		this.DOM.switcher.text(year);
+		this.DOM.year.empty();
+		for (var i = 0, r, m; i < 12; i++) {
+			r = i % 3 ? r : this.DOM.year.append("tr");
+			m = r.append("td");
+			(function (m, i) { setTimeout(function () {
+				m.add("button", LC.MONTHS[i].slice(0, 3) + " '" + (''+year).slice(-2)).hide().fadeIn({ duration: 300 });
+			}, i * 15); })(m, i); }
+		this.DOM.main.anim({ height: this.DOM.switcher.size().height + this.DOM.year.size().height });
+	},
 
 	month: function (year, month) {
 		var date = new Date(year, month, 1);
@@ -54,17 +94,23 @@ new u.Module("calendar", { version: .1, hasCSS: !0 },
 		last = new Date(new Date(year, month + 1, 1) - 86400000).getDate(); day <= last; i++) {
 			w = i % 7 ? w : this.DOM.month.append("tr");
 			d = w.append("td");
-			if (i >= firstWeekDay)
-				(function (d, day) { setTimeout(function () {
-					d.append("button", day).up().hide().fadeIn({ duration: 300 }); }, day * 15); })(d, day++); }
+			i >= firstWeekDay && (function (d, day) { setTimeout(function () {
+				d.add("button", day).hide().fadeIn({ duration: 300 }); }, day * 15); })(d, day++); }
 		this.DOM.main.anim({ height: this.DOM.switcher.size().height + this.DOM.month.size().height });
 	},
 
-	prevMonth: function () {
-		this.month(this.current.year, this.current.month - 1); },
+	prevPage: function () {
+		this.DOM.month.size().height &&
+			this.month(this.current.year, this.current.month - 1) ||
+		this.DOM.year.size().height &&
+			this.year(this.current.year - 1); },
 
-	nextMonth: function () {
-		this.month(this.current.year, this.current.month + 1); }
+	nextPage: function () {
+		this.DOM.month.size().height &&
+			this.month(this.current.year, this.current.month + 1) ||
+		this.DOM.year.size().height &&
+			this.year(this.current.year + 1);
+	}
 })},
 
 // methods for elements
