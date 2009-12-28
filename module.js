1a9c889383b37aeca8bbb89479449297fc48f023
Evandro Myller <evandromyller@gmail.com>
2009-12-28 19:06:27 -0300

    elements reorganized, gave some love to accessibility (tab navigation)

--- module.js
+++ module.js
@@ -11,79 +11,107 @@ var LC = {
 new u.Module("calendar", { version: .1, hasCSS: !0 },
 // core
 { Calendar: u.Class({
-	current: {
-		year: new Date().getFullYear(),
-		month: new Date().getMonth(),
-		day: new Date().getDate(),
-		date: new Date },
+	current: {},
 
 	__init__: function (field) {
 		(this.element = field).calendar = this;
 		this.DOM = { main: u.DOM.create("div.u-calendar").hide() };
-		this.DOM.switcher = this.DOM.main.append("div.u-calendar-switcher");
+		this.DOM.switcher = this.DOM.main.append("div.u-calendar-switcher").append("button");
+		this.DOM.buttons = this.DOM.main.append("div.u-calendar-buttons");
+		this.DOM.buttons.prev = this.DOM.buttons.append("button.u-calendar-buttons-prev", "«");
 		this.DOM.decade = this.DOM.main.append("table.u-calendar-decade");
 		this.DOM.year = this.DOM.main.append("table.u-calendar-year");
 		this.DOM.month = this.DOM.main.append("table.u-calendar-month");
-		this.DOM.buttons = this.DOM.main.append("div.u-calendar-buttons");
-		this.DOM.buttons.prev = this.DOM.buttons.append("button.u-calendar-buttons-prev", "«");
 		this.DOM.buttons.next = this.DOM.buttons.append("button.u-calendar-buttons-next", "»");
 
 		for (var w = -1, weeks = this.DOM.month.append("tr"); LC.DAYS[++w];)
 			weeks.append("th", LC.DAYS[w].slice(0, 2));
 
 		var this_ = this;
-		u(field).on('focus', function () { this_.open(); });
+		u(field).on('focus', function () { this_.open() });
 		this.DOM.buttons.prev.on('click', function () { this_.prevPage(); });
 		this.DOM.buttons.next.on('click', function () { this_.nextPage(); });
 		this.DOM.main.on('click', function (e) { this_.select(e); });
-		this.DOM.switcher.on('click', function () {
-			if (this_.current.month != undefined)
-				this_.DOM.month.suck({ duration: 300 }, function () {
-					this_.DOM.year.show();
-					this_.year(this_.current.year); });
-		});
 	},
 
 	open: function () {
 		u("body").append(this.DOM.main).pos(['left', 'bottom+height'], this.element).show();
-		!this.DOM.switcher.text() && this.goTo(this.current.date); },
+		!this.current.month && this.goTo(new Date); },
 
 	close: function () {
 		this.DOM.main.fadeOut({ duration: 200, destroy: !0 }); },
 
+	goTo: function (date) {
+		this.month(date.getFullYear(), date.getMonth(), date.getDate()); },
+
 	select: function (e) {
 		var this_ = this;
-		if (e.target.parentNode.nodeName == "TD")
+		if (e.target.parentNode.nodeName == "TD") {
 			if (this.current.month != undefined) {
 				this.current.day = +u(e.target).text();
 				this.element.value = LC.formatDate(new Date(this.current.year, this.current.month, this.current.day));
+				this.element.focus();
 				this.close(); }
 			else
 			if (this.current.year != undefined)
 				this.DOM.year.puff({ duration: 300 }, function () {
 					this_.DOM.month.show();
-					this_.month(this_.current.year, u("button", this_.DOM.year).indexOf(e.target))
-				})
+					this_.month(this_.current.year, u("button", this_.DOM.year).indexOf(e.target), 1);
+				});
+			else
+				this.DOM.decade.puff({ duration: 300 }, function () {
+					this_.DOM.year.show();
+					this_.year(+u(e.target).text());
+				}); }
+		else
+		if (e.target.parentNode == this.DOM.switcher.up()[0])
+			if (this.current.month != undefined)
+				this.DOM.month.suck({ duration: 300 }, function () {
+					this_.DOM.year.show();
+					this_.year(this_.current.year); });
+			else
+			if (this.current.year != undefined)
+				this.DOM.year.suck({ duration: 300 }, function () {
+					this_.DOM.decade.show();
+					this_.decade(this_.current.year); });
 	},
 
-	goTo: function (date) {
-		this.month(date.getFullYear(), date.getMonth()); },
+	decade: function (year, selectYear) {
+		this.current.decade = year - year % 20;
+		this.current.year = undefined;
+		this.current.month = undefined;
+		var from = this.current.decade + 1;
+		selectYear = selectYear || from;
+		this.DOM.switcher.text(from + " - " + (from + 19));
+		this.DOM.decade.empty();
+		for (var i = 0, r, y; i < 20; i++) {
+			r = i % 5 ? r : this.DOM.decade.append("tr");
+			y = r.append("td").append("button", from + i).hide();
+			(function (y, i) { setTimeout(function () {
+				y.fadeIn({ duration: 300 },
+				selectYear == from + i ? function () { y[0].focus(); } : undefined);
+			}, i * 10); })(y, i); }
+		this.DOM.main.anim({ height: this.DOM.switcher.size().height + this.DOM.decade.size().height });
+	},
 
-	year: function (year) {
+	year: function (year, selectMonth) {
+		selectMonth = selectMonth || 0;
 		this.current.year = year;
 		this.current.month = undefined;
 		this.DOM.switcher.text(year);
 		this.DOM.year.empty();
 		for (var i = 0, r, m; i < 12; i++) {
 			r = i % 3 ? r : this.DOM.year.append("tr");
-			m = r.append("td");
+			m = r.append("td").append("button", LC.MONTHS[i].slice(0, 3) + " '" + (''+year).slice(-2)).hide();
 			(function (m, i) { setTimeout(function () {
-				m.add("button", LC.MONTHS[i].slice(0, 3) + " '" + (''+year).slice(-2)).hide().fadeIn({ duration: 300 });
-			}, i * 15); })(m, i); }
+				m.fadeIn({ duration: 300 },
+				selectMonth == i ? function () { m[0].focus(); } : undefined);
+			}, i * 10); })(m, i); }
 		this.DOM.main.anim({ height: this.DOM.switcher.size().height + this.DOM.year.size().height });
 	},
 
-	month: function (year, month) {
+	month: function (year, month, selectDay) {
+		selectDay = selectDay || 1;
 		var date = new Date(year, month, 1);
 		this.current.year = date.getFullYear();
 		this.current.month = date.getMonth();
@@ -93,24 +121,31 @@ new u.Module("calendar", { version: .1, hasCSS: !0 },
 		firstWeekDay = date.getDay(),
 		last = new Date(new Date(year, month + 1, 1) - 86400000).getDate(); day <= last; i++) {
 			w = i % 7 ? w : this.DOM.month.append("tr");
-			d = w.append("td");
+			d = w.append("td").append("button", day).hide();
 			i >= firstWeekDay && (function (d, day) { setTimeout(function () {
-				d.add("button", day).hide().fadeIn({ duration: 300 }); }, day * 15); })(d, day++); }
+				d.fadeIn({ duration: 300 },
+				selectDay == day ? function () { d[0].focus(); } : undefined);
+			}, day * 10); })(d, day++); }
 		this.DOM.main.anim({ height: this.DOM.switcher.size().height + this.DOM.month.size().height });
 	},
 
 	prevPage: function () {
-		this.DOM.month.size().height &&
-			this.month(this.current.year, this.current.month - 1) ||
-		this.DOM.year.size().height &&
-			this.year(this.current.year - 1); },
+		if (this.current.month != undefined)
+			this.month(this.current.year, this.current.month - 1);
+		else
+		if (this.current.year != undefined)
+			this.year(this.current.year - 1);
+		else
+			this.decade(this.current.decade - 1); },
 
 	nextPage: function () {
-		this.DOM.month.size().height &&
-			this.month(this.current.year, this.current.month + 1) ||
-		this.DOM.year.size().height &&
+		if (this.current.month != undefined)
+			this.month(this.current.year, this.current.month + 1);
+		else
+		if (this.current.year != undefined)
 			this.year(this.current.year + 1);
-	}
+		else
+			this.decade(this.current.decade + 21); }
 })},
 
 // methods for elements
